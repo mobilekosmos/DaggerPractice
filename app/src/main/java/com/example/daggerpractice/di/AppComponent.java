620058dc75d6bbb7a7907acaaa21387457269a57
David Bauer <david.bauer@sophos.com>
2019-10-21 15:28:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/daggerpractice/di/AppComponent.java
@@ -0,0 +1,32 @@
+package com.example.daggerpractice.di;
+
+import android.app.Application;
+
+import com.example.daggerpractice.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        // Needed when you use the Android stuff
+        modules = {
+                AndroidSupportInjectionModule.class
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+    // You can think of AppCompoenent as a Service and BaseApplication would be the client. Using AndroidInjector<BaseApplication>
+    // you are telling Dagger to inject BaseApplication into this component.
+
+    @Component.Builder
+    interface Builder {
+
+        // Optional, the name can be anything.
+        @BindsInstance
+        Builder application(Application application);
+
+        // This line is mandatory
+        AppComponent build();
+    }
+}
